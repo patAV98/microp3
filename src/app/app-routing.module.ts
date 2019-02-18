72024096b14a58378a4874f69cb427e9f972e80f
patAV98 <kh.ff.love98@gmail.com>
2019-02-18 15:54:50 +0100

    Microp3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'agenda-electronica', loadChildren: './agenda-electronica/agenda-electronica.module#AgendaElectronicaPageModule' },
+  { path: 'nuevo-evento', loadChildren: './nuevo-evento/nuevo-evento.module#NuevoEventoPageModule' },
+  { path: 'libreta-contactos', loadChildren: './libreta-contactos/libreta-contactos.module#LibretaContactosPageModule' },
+  { path: 'nuevo-contacto', loadChildren: './nuevo-contacto/nuevo-contacto.module#NuevoContactoPageModule' },
+  { path: 'acerca-de', loadChildren: './acerca-de/acerca-de.module#AcercaDePageModule' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
+      title: 'Lista de Contactos',
+      url: '/agenda-electronica',
       icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
+      },
+      {
+      title: 'Agenda Electrónica',
+      url: '/libreta-contactos',
+      icon: 'list'
+      },
+      {
+      title: 'Acerca de',
+      url: '/acerca-de',
       icon: 'list'
     }
   ];
